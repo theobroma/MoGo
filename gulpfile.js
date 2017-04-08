9711f1aea84000e9d5b48ca1f1b47c9b79c2bb3f
Alexandr <theobroma222@gmail.com>
2017-04-09 00:26:55 +0300

    Initial

---
+++ gulpfile.js
@@ -0,0 +1,140 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    rimraf = require('rimraf'),
+    notify = require('gulp-notify'),
+    plumber = require('gulp-plumber'),
+    sourcemaps = require('gulp-sourcemaps'),
+    runSequence = require('run-sequence'),
+	  pug = require('gulp-pug'),
+    //CSS
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    //Server
+    browserSync = require('browser-sync').create(),
+    // plugins for build
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    csso = require('gulp-csso');
+
+//Конфиг
+var paths = {
+  src: './app/',
+  out: './app/',
+  dist: './dist/'
+}
+
+//Компиляция SASS
+gulp.task('sass', function () {
+   return gulp.src([paths.src + 'sass/**/*.{sass,scss}', '!' + paths.src + 'sass/**/_*.{sass,scss}'])
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError)) // plumber doesn't work here
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.out + '/css/'))
+    .pipe(browserSync.stream())
+    .pipe(notify("SASS Compiled"));
+});
+//Компиляция Pug
+gulp.task('pug', function () {
+    return gulp.src([paths.src + 'pug/*.pug', '!' + paths.src + 'pug/_*.pug'])
+    .pipe(plumber())
+    .pipe(pug({pretty: true}))
+    .pipe(gulp.dest(paths.out))
+    .pipe(browserSync.stream())
+    .pipe(notify("Pug Compiled"));
+});
+//Конкатанация JavaScript
+gulp.task('concat', function() {
+  return gulp.src(paths.src+'/javascript/**/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest(paths.out + '/js/'))
+    .pipe(browserSync.stream())
+    .pipe(notify("Scripts concatenated"));
+});
+
+
+
+//Перезагрузка страницы
+gulp.task("browser-sync", function() {
+        browserSync.init(["css/*.css", "js/*.js","*.html"], {
+            server: {
+                baseDir: paths.out
+            }
+        });
+    });
+/*
+# ===============================================
+# Создание билда проэкта
+# ===============================================
+*/
+
+//clean build folder
+gulp.task('cleanBuildDir', function (cb) {
+    rimraf(paths.dist, cb);
+});
+
+//minify images
+gulp.task('imgBuild', function () {
+    return gulp.src(paths.src + 'img/**/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest(paths.dist + 'img'))
+});
+
+//minify css
+gulp.task('cssBuild', function () {
+    return gulp.src(paths.out + 'css/**/*')
+        .pipe(csso())
+        .pipe(gulp.dest(paths.dist + 'css/'))
+});
+//minify js
+gulp.task('jsBuild', function() {
+  return gulp.src(paths.src+'/javascript/**/*.js')
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.dist + '/js/'))
+});
+
+//copy fonts
+gulp.task('fontsBuild', function() {
+  return gulp.src(paths.src+'/fonts/**/*.*')
+    .pipe(gulp.dest(paths.dist + '/fonts/'))
+});
+
+//copy html
+gulp.task('htmlBuild', function() {
+  return gulp.src(paths.src+'/*.html')
+    .pipe(gulp.dest(paths.dist))
+});
+
+/*
+# ===============================================
+# Отслеживание изменения файлов
+# ===============================================
+*/
+
+gulp.task('watch', function() {
+    gulp.watch(paths.src + '/sass/**/*.{sass,scss}', ['sass']);
+    gulp.watch(paths.src + '/javascript/**/*.js', ['concat']);
+    gulp.watch(paths.src + '/pug/**/*.pug', ['pug']);
+});
+
+/*
+# ===============================================
+# Группы тасков
+# ===============================================
+*/
+
+gulp.task('compile', ['sass','pug','concat']);
+gulp.task('build', function(callback) {
+  runSequence('cleanBuildDir','compile',['jsBuild', 'cssBuild', 'fontsBuild', 'htmlBuild', 'imgBuild'], callback);
+});
+gulp.task('default', ['compile','browser-sync','watch']);
