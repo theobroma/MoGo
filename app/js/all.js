16e9cbb8d56944b285e0a9113a9aa20298cefaff
Alexandr <theobroma222@gmail.com>
2017-04-27 13:16:49 +0300

    navbar

--- app/js/all.js
+++ app/js/all.js
@@ -1,2 +1,13 @@
+//burger menu
+$(".burger-menu").click(function () {
+  $(this).toggleClass("menu-on");
+  $(".mynavbar-collapse").fadeToggle("in");
+});
 
+//smooth parallax
+$(document).scroll(function () {
+
+  var $movebg = $(window).scrollTop() * -0.3;
+  $('.portion').css('background-positionY', ($movebg) + 'px');
+});
 
