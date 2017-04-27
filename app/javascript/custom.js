86d0d24ff9b5c1ca1779f00b57d33c135447f91c
Alexandr <theobroma222@gmail.com>
2017-04-27 14:53:31 +0300

    add scroll toTop

--- app/javascript/custom.js
+++ app/javascript/custom.js
@@ -7,6 +7,23 @@ $(".burger-menu").click(function () {
 //smooth parallax
 $(document).scroll(function () {
 
-  var $movebg = $(window).scrollTop() * -0.3;
+  var $movebg = $(window).scrollTop() * -0.5;
   $('.portion').css('background-positionY', ($movebg) + 'px');
+});
+
+//scroll toTop
+$('#toTop').click(function () {
+  $('body').animate({
+    scrollTop: 0
+  }, 1000);
+  return false;
+});
+
+$(window).scroll(function(){                              // отслеживаем событие
+   if ( $(window).scrollTop() <= 200 ){                   // ставим условие
+      $('#toTop').css('opacity','0').css('bottom','-50px');            // определяем действие
+   }
+    if ( $(window).scrollTop() >= 200 ){
+      $('#toTop').css('opacity','0.7').css('bottom','30px');
+   }
 });
